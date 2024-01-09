ce85d94038daad55cd58eb66ecf2c12e80307fe1
ganesh79934 <154883577+ganesh79934@users.noreply.github.com>
2024-01-09 16:29:24 +0530

    Add files via upload

--- Test111.java
+++ Test111.java
@@ -1,27 +1,29 @@
-class Parent
+import java.io.*;
+import java.util.*;
+class  Test111
 {
-void surname()
-{
-System.out.println("k");
-}
-void designation()
-{
-System.out.println("clerk");
-}
-}
-class Child extends Parent
-{
-void designation()
-{
-System.out.println("java programmer");
-}
-}
-class Test
-{
-public static void main(String args[])
-{
-Child c1=new Child();
-c1.surname();
-c1.designation();
-}
+	public static void main(String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		int admno,mat,phy,che,tot,avg;
+		String name;
+		System.out.println("Enter admno");
+		admno=s.nextInt();
+		System.out.println("Enter name");
+		name=s.nextInt();
+		System.out.println("Enter 3 subject marks");
+		mat=s.nextInt();
+		phy=s.nextInt();
+		che=s.nextInt();
+		tot=mat+phy+che;
+		avg=tot/3;
+		printWrter p1=new printWriter("student.txt");
+		p1.println("Adimission no="+admno);
+        p1.println("Name="+name);
+		p1.println("Maths="+mat);
+		p1.println("Physics="+phy);
+		p1.println("Chemistry="+che);
+		p1.println("TOtal="+tot);
+		p1.println("Average="+avg);
+	}
 }
