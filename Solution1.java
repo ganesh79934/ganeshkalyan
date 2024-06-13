c3517657b2dee6012845c4e14e3bd5cf3a8270ac
ganesh79934 <154883577+ganesh79934@users.noreply.github.com>
2024-06-13 15:07:29 +0530

    Add files via upload

---
+++ Solution1.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class 
+
+ {
+
+    public static void main(String[] args) {
+        
+    Scanner sc=new Scanner(System.in);
+        String name=sc.nextLine();
+        System.out.println("welcome "+name);
+    }
+}
