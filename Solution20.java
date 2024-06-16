25b64bfa682997d28960be2c87aacf7bf499b86a
ganesh79934 <154883577+ganesh79934@users.noreply.github.com>
2024-06-16 06:45:44 +0530

    Add files via upload

---
+++ Solution20.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+ Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        double b=a*3.14;
+    
+        System.out.println(b);
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
